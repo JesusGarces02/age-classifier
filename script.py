2f0008e42f60bbc05b3d23f5b8c23a98e33aa9b5
Santiagokmids <72984873+Santiagokmids@users.noreply.github.com>
2024-05-14 14:48:11 -0500

    Update script.py

--- script.py
+++ script.py
@@ -3,10 +3,14 @@ import os
 import numpy as np
 import flask
 import joblib
+import cv2
 from flask import Flask, render_template, request
 
 #creating instance of the class
 app=Flask(__name__)
+
+#initializing camera
+camera = cv2.VideoCapture(0)
 
 #to tell flask what url shoud trigger the function index()
 @app.route('/')
@@ -28,12 +32,43 @@ def finalResult():
         f.save(os.path.join(upload_directory, f.filename)) 
         return render_template("final-result.html", name = f.filename)  
 
-def ValuePredictor(to_predict_list):
-    to_predict = np.array(to_predict_list).reshape(1, 4)
+def ValuePredictor(filename):
     loaded_model = joblib.load("checkpoints/model.pkl")
-    result = loaded_model.predict(to_predict)
+    result = loaded_model.predict(os.path.join(app.root_path+"/static/", filename))
 
     return result[0]
+
+@app.route('/camera')
+def camera():
+    return flask.render_template("camera.html")
+
+#Ref: https://parzibyte.me/blog/2021/02/10/python-acceder-camara-web-opencv-flask/ 
+#@app.route("/streaming_camara")
+#def streaming_camara():
+    #return Response(generador_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+#function to get one frame of the camera using opencv(cv2)
+def getFrameCamera():
+    isOk, frame = camera.read()
+
+    if not isOk:
+        return False, None
+    
+    _, bufer = cv2.imencode(".jpg", frame)
+    image = bufer.tobytes()
+
+    return True, image
+
+#function to get frames of the camera, like streaming
+def framesGenerator():
+    while True:
+        isOk, image = getFrameCamera()
+        if not isOk:
+            break
+
+        else:
+            # Regresar la imagen en modo de respuesta HTTP
+            yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + image + b"\r\n"
 
 @app.route('/result',methods = ['POST'])
 def result():
